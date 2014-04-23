6f62a5cb766106903baf3630a402eb8eb89436ee
Jonah Bishop <git@borngeek.com>
2014-04-23 11:33:14 -0400

    Substantial updates to the build script

--- build.pl
+++ build.pl
@@ -1,44 +1,84 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+
 use Cwd;
 use File::Basename;
 use Getopt::Long;
 
+our $VERSION = "1.01";
+
 # An array of relative file paths in which a version number should be updated
 my @VERSION_FILES = qw/chrome\\content\\about.xul install.rdf/;
 
-my $jarFile = "googlebarlite.jar";
-my $outputFilename = "googlebarlite.xpi";
-my $version = '';
+my $bumpVersion = 0;
+my $explicitVersion = '';
+my $outputFilename = '';
+my $outputFolder = "output";
+my $outputShortname = "googlebarlite";
 
-GetOptions("output=s" => \$outputFilename,
-		   "version=s" => \$version,
+GetOptions("bump" => \$bumpVersion,
+		   "output=s" => \$outputFilename,
+		   "version=s" => \$explicitVersion,
 		   "help" => sub { usageMessage() });
 
-print "+--------------------------------+\n";
-print "| Firefox Extension Build Script |\n";
-print "| Written by Jonah Bishop        |\n";
-print "+--------------------------------+\n";
+print "\nGooglebar Lite Build Script v$VERSION\n";
+print "Written by Jonah Bishop\n\n";
 print "Building extension...\n";
 my $homeDir = getcwd();
 
-if (!$version)
+if (! -d "$homeDir/$outputFolder")
 {
-	print "Skipping version update.\n";
+	if(! mkdir "$homeDir/$outputFolder")
+	{
+		print "ERROR: Failed to create $homeDir/$outputFolder: $!";
+		exit 1;
+	}
 }
-else
+
+my $versionMajor = 0;
+my $versionMinor = 0;
+my $versionRev = 0;
+
+scanForVersion();
+
+if ($bumpVersion == 1)
 {
+	# Bump only the versionRev portion if the user wanted to bump
+	$versionRev++;
 	updateVersion();
+}
+elsif ($explicitVersion ne '')
+{
+	grabVersionPieces($explicitVersion);
+	updateVersion();
+}
+
+if (! $outputFilename)
+{
+	$outputFilename = "${outputShortname}_${versionMajor}_${versionMinor}_${versionRev}.xpi";
+	print " - Output will be written to: $outputFilename\n";
 }
 
 # Create the XPI file
 print "\nCreating XPI file...\n";
-system("zip -r $outputFilename -\@ < xpizip.txt");
+system("zip -r $outputFolder/$outputFilename -\@ < xpizip.txt");
+
+print "\nExtension package created successfully\n";
+exit 0;
 
 # ======================================================================
 # End Main Script --- Begin Subroutines
 # ======================================================================
+
+sub grabVersionPieces
+{
+	my $vs = shift;
+	my @pieces = split /\./, $vs;
+	$versionMajor = $pieces[0] || 0;
+	$versionMinor = $pieces[1] || 0;
+	$versionRev = $pieces[2] || 0;
+}
 
 sub parseVersionFile
 {
@@ -52,24 +92,34 @@ sub parseVersionFile
 	
 	foreach my $statement (@lines)
 	{
-		chomp $statement;
-		if ($statement =~ m/^(\s*?)<em:version>/)
-		{
-			my $whitespace = $1;
-			print $out "$whitespace<em:version>$version</em:version>\n";
-		}
-		elsif($statement =~ m/value="Version [0-9\.]+?"/)
-		{
-			$statement =~ s/value="[^"]+?"/value="Version $version"/;
-			print $out "$statement\n";
-		}
-		else
-		{
-			print $out "$statement\n";
-		}
+		# Handle the install.rdf case
+		$statement =~ s!<em:version>[0-9\.]+</em:version>!<em:version>$versionMajor.$versionMinor.$versionRev</em:version>!;
+		
+		# Handle the about.xul case
+		$statement =~ s!value="Version [0-9\.]+"!value="Version $versionMajor.$versionMinor.$versionRev"!;
+		
+		print $out $statement;
 	}
 	
 	close $out;
+}
+
+sub scanForVersion
+{
+	print " - Scanning for version string\n";
+	
+	open my $in, '<', 'install.rdf' or die "Cannot open install.rdf to scan version string: $!";
+	while (<$in>)
+	{
+		if (m!<em:version>([0-9\.]+)</em:version>!)
+		{
+			my $vs = $1;
+			print "   Found version string: $vs\n\n";
+			grabVersionPieces($vs);
+			last;
+		}
+	}
+	close $in;
 }
 
 sub updateVersion
@@ -95,10 +145,18 @@ sub updateVersion
 
 sub usageMessage
 {
-	print "Options:\n";
-	print "  --output <FILENAME>\n";
-	print "    Specify the output XPI filename\n\n";
-	print "  --version <VERSION>\n";
-	print "    Specify the version string to use for this extension\n";
+	print <<USAGE;
+Options:
+  --bump
+    Bumps the revision version portion of the version string (i.e. the third
+	value: X.Y.Z)
+	
+  --output <FILENAME>
+    Explicitly sets the output XPI filename
+	
+  --version <VERSION>
+    Explicitly sets the version string to use
+USAGE
+
 	exit();
 }
